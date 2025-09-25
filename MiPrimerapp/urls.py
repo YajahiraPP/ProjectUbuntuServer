ef5485cc8599bbe47114417e024269c2b6c61c8e
Yajahira <13916@ulsachihuahua.edu.mx>
2025-09-24 18:54:29 -0600

    ...

---
+++ MiPrimerapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('suma/', views.suma, name='suma'),
+    path('<str:nombre>', views.saludo, name='saludo')
+]
--- MiPrimerapp/views.py
+++ MiPrimerapp/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 
 def index(request):
 	return  HttpResponse("Hello, World!")
+
+def suma(request):
+    resultado = None  
+
+    if request.method == "POST":
+        num1 = int(request.POST.get("num1", 0))
+        num2 = int(request.POST.get("num2", 0))
+        resultado = num1 + num2  
+
+    return render(request, "MiPrimerapp/suma.html", {"resultado": resultado})
+
+
+def saludo(request, nombre):
+    return render(request, "MiPrimerapp/saludo.html", {"nombre": nombre.capitalize()})
