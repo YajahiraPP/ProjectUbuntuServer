002bfc05d97ee9f1fb3f6f2bd4455c41cbff8ad2
Yajahira <13916@ulsachihuahua.edu.mx>
2025-10-06 20:01:03 -0600

    ...

--- MiPrimerapp/urls.py
+++ MiPrimerapp/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('suma/', views.suma, name='suma'),
-    path('<str:nombre>', views.saludo, name='saludo')
+    path('<str:nombre>', views.saludo, name='saludo'),
+    path('about/', views.about, name='about'),
+    path('tasks/', views.tasks_index, name='tasks_index'),
+    path('tasks/add/', views.tasks_add, name='tasks_add'),
+    path('tasks/admin/', views.tasks_admin_list, name='tasks_admin_list'),
+    path("index2/",views.index2,name="index2"),
 ]
--- MiPrimerapp/views.py
+++ MiPrimerapp/views.py
@@ -22,4 +22,21 @@ def suma(request):
 def saludo(request, nombre):
     return render(request, "MiPrimerapp/saludo.html", {"nombre": nombre.capitalize()})
 
-from .models import Task
+def about(request):
+    return render(request, "MiPrimerapp/about.html")
+
+def index2(request):
+    return render(request, "MiPrimerapp/index2.html")
+
+def tasks_index(request):
+    return render(request, "MiPrimerapp/tasks_index.html", {"tasks": tasks})
+
+def tasks_add(request):
+    if request.method == "POST":
+        new_task = request.POST.get("task")
+        if new_task:
+            tasks.append(new_task)
+    return render(request, "MiPrimerapp/tasks_add.html")
+
+def tasks_admin_list(request):
+    return render(request, "MiPrimerapp/tasks_admin_list.html", {"tasks": tasks})
